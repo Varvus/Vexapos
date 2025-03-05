38bed76fec57b91ab82255fd423f9316bbcc2229
Varvus <45293461+Varvus@users.noreply.github.com>
2025-03-05 11:44:37 -0600

    mensaje de alerta al no obtener usuario

--- admin-producto.php
+++ admin-producto.php
@@ -18,6 +18,11 @@
         <?php
         $cve_usuario = $_GET["wk_usua"]; // Este valor puede ser dinámico dependiendo de tu implementación
 
+        if ($cve_usuario == ""){
+            echo '<div class="alert alert-danger">ACCESO DENEGADO</div>';
+            return;
+        }
+
         // Consulta SQL
         $sql = "SELECT 
                     p.cve_producto, 
