cc6964c28b10efcfd25c6e7467b3653400b27712
Varvus <45293461+Varvus@users.noreply.github.com>
2025-04-10 14:32:38 -0600

    Fixed issues ok product

--- admin-menu.php
+++ admin-menu.php
@@ -1,24 +1,27 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><h1 class="m-0">Navbar</h1></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <a class="navbar-brand" href="#">
+            <h1 class="m-0">Navbar</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-0 mb-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="admin-dashboard.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="admin-producto.php">Producto</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="admin-venta.htm">Venta</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Salir</a>
-            </li>
-        </ul>
+            <ul class="navbar-nav me-auto mb-0 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin-dashboard.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin-producto.php">Producto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin-venta.php">Venta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Salir</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
